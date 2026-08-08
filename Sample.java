c695db04b1c688f47aac329e5e3b8cad841aa134
aiseyaaa <isiahmiguelfromyt@gmail.com>
2026-08-08 19:57:49 +0800

    Implement main method to read user name
    Added main method to Sample class for user input.

---
+++ Sample.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sample{
+  public static void main(String [] args){
+Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter your name: ");
+    String name = sc.nextLine();
+    
+  }
+}
